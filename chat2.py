f8f11235b5bb4f1d8523a4b9fc856421337b3edd
Taka005 <01taka005@gmail.com>
2023-10-22 17:21:34 +0900

    fix

--- chat2.py
+++ chat2.py
@@ -21,23 +21,20 @@ data = "\n".join([tagger.parse(sentence) for sentence in data])
 model = markovify.NewlineText(data)
 
 while True:
-    try:
-        user_input = input("ユーザー: ")
-        tries = 0
-        text = None
-        while tries < 5:
-            start = getStartText(user_input)
-            print(f"試行中: {start}")
+    user_input = input("ユーザー: ")
+    tries = 0
+    text = None
+    while tries < 5:
+        start = getStartText(user_input)
+        print(f"試行中: {start}")
 
-            try:
-                text = model.make_sentence_with_start(beginning=start,tries=100).replace(" ", "")
-                break
-            except:
-                tries += 1
+        try:
+            text = model.make_sentence_with_start(beginning=start,tries=100).replace(" ", "")
+            break
+        except:
+            tries += 1
 
-        if text is None:
-            text = "生成できませんでした"
-    except:
+    if text is None:
         text = "生成できませんでした"
 
     print(f"ボット: {text}")
