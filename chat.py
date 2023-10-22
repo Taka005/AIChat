e80ab3c987dd2ec0e3b147261d5aa84d1dc3f0fa
Taka005 <01taka005@gmail.com>
2023-10-22 16:50:41 +0900

    生成できませんでした

---
+++ chat.py
@@ -0,0 +1,31 @@
+import markovify
+import MeCab
+import json
+import sys
+import random
+
+def getNouns(text):
+    tagger = MeCab.Tagger("-Ochasen")
+    words = tagger.parse(text).split("\n")
+    return [word.split("\t")[0] for word in words if "名詞" in word]
+
+with open("data.json","r",encoding="utf-8") as file:
+    data = json.load(file)
+
+tagger = MeCab.Tagger("-Owakati")
+data = "\n".join([tagger.parse(sentence) for sentence in data])
+model = markovify.NewlineText(data)
+
+while True:
+    user_input = input("ユーザー: ")
+    if not user_input:
+        text = "入力してください"
+    nouns = getNouns(user_input)
+    start = random.choice(nouns)
+    
+    try:
+        text = model.make_sentence_with_start(beginning=start,tries=100).replace(" ","")
+    except:
+        text = "生成できませんでした"
+        
+    print(f"BOT: {text}")
