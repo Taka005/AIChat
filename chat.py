5b325318521e9cb7e973cc5878cb5fe8e9075472
Taka005 <01taka005@gmail.com>
2023-10-22 19:22:08 +0900

    change

--- chat.py
+++ chat.py
@@ -4,15 +4,16 @@ import json
 import sys
 import random
 
-def getNouns(text):
+def getStartText(text):
     tagger = MeCab.Tagger("-Ochasen")
-    words = tagger.parse(text).split("\n")
+    words = tagger.parse(text).split("\n")[:-2]
     nouns = [word.split("\t")[0] for word in words if "名詞" in word]
-    if not nouns:
-        nouns = random.choice(words)
-    return nouns
+    if nouns:
+        return random.choice(nouns)
+    else:
+        return random.choice(words).split("\t")[0]
 
-with open("data.json","r",encoding="utf-8") as file:
+with open("data.json", "r", encoding="utf-8") as file:
     data = json.load(file)
 
 tagger = MeCab.Tagger("-Owakati")
@@ -20,13 +21,19 @@ data = "\n".join([tagger.parse(sentence) for sentence in data])
 model = markovify.NewlineText(data)
 
 while True:
-    try:
-        user_input = input("ユーザー: ")
-        nouns = getNouns(user_input)
-        start = random.choice(nouns)
+    user_input = input("ユーザー: ")
+    tries = 0
+    text = None
+    while tries < 5:
+        start = getStartText(user_input)
+        
+        try:
+            text = model.make_sentence_with_start(beginning=start,tries=100).replace(" ", "")
+            break
+        except:
+            tries += 1
 
-        text = model.make_sentence_with_start(beginning=start,tries=100).replace(" ","")
-    except:
+    if text is None:
         text = "生成できませんでした"
 
     print(f"ボット: {text}")
